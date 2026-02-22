fb1d030f823c695f26e99051e7f4210f56a138ed
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2026-02-22 10:31:20 +0600

    Implement OpenGL truck drawing application
    This program initializes an OpenGL window and draws a simple truck with a cargo, cab, window, and wheels using basic shapes.

---
+++ Projects/simple truck.cpp
@@ -0,0 +1,110 @@
+#include<GL/gl.h>
+#include<GL/glu.h>
+#include<GL/glut.h>
+
+void display();
+void reshape(int,int);
+void timer(int);
+
+void init(){
+    glClearColor(1.0, 1.0, 1.0, 1.0); // White background
+}
+
+// 1. Simple Square Function
+void drawSquare(float r, float g, float b){
+    glColor3f(r, g, b);
+    glBegin(GL_QUADS);
+    glVertex2f(-1, 1); glVertex2f(-1, -1);
+    glVertex2f(1, -1); glVertex2f(1, 1);
+    glEnd();
+
+    // Black Outline
+    glColor3f(0, 0, 0);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(-1, 1); glVertex2f(-1, -1);
+    glVertex2f(1, -1); glVertex2f(1, 1);
+    glEnd();
+}
+
+// 2. Simple Circle Function (Using GLU Disk)
+void drawCircle(float r, float g, float b) {
+    GLUquadric *quad;
+    quad = gluNewQuadric();
+
+    // Fill Color
+    glColor3f(r, g, b);
+    gluDisk(quad, 0, 1, 32, 1); // Inner radius 0, Outer radius 1
+
+    // Black Outline
+    glColor3f(0, 0, 0);
+    gluQuadricDrawStyle(quad, GLU_SILHOUETTE);
+    gluDisk(quad, 0, 1, 32, 1);
+}
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    // CARGO (Blue)
+    glPushMatrix();
+        glTranslatef(2.5, 1.0, 0);
+        glScalef(5.0, 3.5, 1.0);
+        drawSquare(0.2, 0.5, 0.9);
+    glPopMatrix();
+
+    // CAB (Red)
+    glPushMatrix();
+        glTranslatef(-4.5, -0.5, 0);
+        glScalef(2.0, 2.0, 1.0);
+        drawSquare(0.9, 0.2, 0.2);
+    glPopMatrix();
+
+    // WINDOW (Light Blue)
+    glPushMatrix();
+        glTranslatef(-4.2, 0.2, 0);
+        glScalef(1.2, 0.8, 1.0);
+        drawSquare(0.7, 0.9, 1.0);
+    glPopMatrix();
+
+    // FRONT WHEEL
+    glPushMatrix();
+        glTranslatef(-4.0, -2.5, 0);
+        glScalef(1.3, 1.3, 1.0);
+        drawCircle(0.3, 0.3, 0.3);
+    glPopMatrix();
+
+    // REAR WHEEL
+    glPushMatrix();
+        glTranslatef(4.5, -2.5, 0);
+        glScalef(1.3, 1.3, 1.0);
+        drawCircle(0.3, 0.3, 0.3);
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h){
+    glViewport(0,0,w,h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-10,10,-10,10);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void timer(int){
+    glutPostRedisplay();
+    glutTimerFunc(1000/60, timer, 0);
+}
+
+int main(int argc, char**argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Lab task");
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutTimerFunc(0, timer, 0);
+    glutMainLoop();
+    return 0;
+}
